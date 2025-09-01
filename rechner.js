732bff3d77cc9baa72682b173b301a0b3c729168
Lias Albiez <liasalbiez@gmail.com>
2025-09-01 20:05:23 +0200

    Update rechner.js

--- rechner.js
+++ rechner.js
@@ -1,11 +1,12 @@
-// Warten, bis die Seite vollständig geladen ist
+
 window.addEventListener('DOMContentLoaded', function () { 
-  const startBtn = document.getElementById('startButton'); //sucht, benennt button namens ("startButton")
-  const startScreen = document.getElementById('startScreen');//sucht, benennt Screen namens ("startSceen")
-  const calculator = document.getElementById('calculatorContainer');//sucht, benennt button namens ("ccalculatorContainer")
+  const startBtn = document.getElementById('startButton'); 
+  const startScreen = document.getElementById('startScreen');
+  const calculator = document.getElementById('calculatorContainer');
 
   startBtn.addEventListener('click', function () {
     startScreen.style.display = 'none'; //Versteckt Startbildschirm
     calculator.style.display = 'flex'; //zeigt Rechner
   });
 });
+
